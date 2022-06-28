6ae71dd43c198c0957486f2fc3ded9149c9d7108
Berry <78561662+Berry-hub@users.noreply.github.com>
2022-06-28 20:48:39 +0800

    small formating and colour change

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QListWidget, QMessageBox
 from PyQt6.QtGui import QIcon, QFont, QPixmap
+from PyQt6.QtCore import Qt
 import sys
 
 
@@ -24,21 +25,24 @@ class Window(QWidget):
         self.loan_amount = QLineEdit('Total loan amount', self)    
         self.interest_rate = QLineEdit('Annual interest rate', self)
         self.duration_years = QLineEdit('Duration in years', self)
+        for item in (self.loan_amount, self.interest_rate, self.duration_years):
+            item.setFont(QFont('Arial', 11))
 
         self.btn = QPushButton('Count monthly payment', self)
-        self.btn.setStyleSheet('background-color: pink')
+        self.btn.setStyleSheet('background-color: coral')
         self.btn.setFont(QFont('Arial', 12))
         self.result_payment = QLabel('', self)
 
         self.reset_btn = QPushButton('RESET', self)
-        self.reset_btn.setStyleSheet('background-color: red')
+        self.reset_btn.setStyleSheet('background-color: crimson')
         self.reset_btn.setFont(QFont('Arial', 12))
 
         self.month_label = QLabel('month', self)
         self.interest_label = QLabel('interest', self)
         self.amortization_label = QLabel('amortization', self)
-        # self.label.setStyleSheet('background-color: transparent')
-        # self.label.setFont(QFont('Arial', 12))
+        for item in (self.month_label, self.interest_label, self.amortization_label):
+            item.setFont(QFont('Arial', 11))
+            item.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
 
         self.anum = QLabel('', self)
         self.interest_annual = QLabel('', self)
@@ -53,7 +57,7 @@ class Window(QWidget):
         self.box.addWidget(self.interest_rate, 1, 0)
         self.box.addWidget(self.duration_years, 2, 0)
         self.box.addWidget(self.btn, 1, 1)
-        self.box.addWidget(self.reset_btn, 7, 3)
+        self.box.addWidget(self.reset_btn, 7, 2)
         self.box.addWidget(self.result_payment, 2, 1)
         self.box.addWidget(self.month_label, 4, 0)
         self.box.addWidget(self.interest_label, 4, 1)
@@ -80,7 +84,7 @@ class Window(QWidget):
             if loan <= 0 or interest <= 0 or duration <= 0:
                 raise ValueError
             monthly_payment = loan * (interest * (1.0 + interest) ** duration) / ((1.0 + interest) ** duration - 1.0)
-            show_monthly_payment = "{:.2f} $".format(monthly_payment)
+            show_monthly_payment = "{:,.2f} $".format(monthly_payment)
             self.result_payment.setText(show_monthly_payment)
             self.result_payment.setFont(QFont('Arial', 18))
 
@@ -95,16 +99,16 @@ class Window(QWidget):
             list_interest_str  = []
             list_amortization_str = []
             for value in list_interest:
-                list_interest_str.append(str("{:.2f} $".format(value)))
+                list_interest_str.append(str("{:,.2f} $".format(value)))
             for value in list_amortization:
-                list_amortization_str.append(str("{:.2f} $".format(value)))
+                list_amortization_str.append(str("{:,.2f} $".format(value)))
             self.show_interest.addItems(list_interest_str)
             self.show_amortization.addItems(list_amortization_str)
             self.show_month.addItems(list_month)
 
             self.anum.setText('per anum')
-            self.interest_annual.setText(str('{:.2f} $'.format(sum(list_interest))))
-            self.amortization_annual.setText(str('{:.2f} $'.format(sum(list_amortization))))
+            self.interest_annual.setText(str('{:,.2f} $'.format(sum(list_interest))))
+            self.amortization_annual.setText(str('{:,.2f} $'.format(sum(list_amortization))))
             self.anum.setFont(QFont('Arial', 14))
             self.interest_annual.setFont(QFont('Arial', 14))
             self.amortization_annual.setFont(QFont('Arial', 14))
@@ -123,8 +127,6 @@ class Window(QWidget):
         self.show_amortization.clear()
         self.interest_annual.clear()
         self.amortization_annual.clear()
-
-
 
 
         ### can upgrade some more calculations ###
